3d016a002f2eefa59b3e4be7b46cda4e397539f5
Yuan Du <yuandu.cs@gmail.com>
2013-07-11 03:58:22 +0000

    updated grader.js to parse url

--- grader.js
+++ grader.js
@@ -26,6 +26,8 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://glacial-basin-5642.herokuapp.com";
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -40,8 +42,39 @@ var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+var gethtml = function(data){
+//console.log(data);
+    return data;
+
+};
+
+var cheerioURL = function(url){
+  
+ 
+  // rest.get(url).on('complete',function(data){console.log(data);});
+   rest.get(url).on('complete',function(data){gethtml(data);});
+//return rest.get(url).on('complete',function(data){return cheerio.load(data);});
+   return gethtml;
+
+};
+
+
+
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var checkURL = function(url,checksfile){
+ $ = cheerioURL(url);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    console.log("url checking ..." );
+    return out;
+
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
@@ -60,13 +93,23 @@ var clone = function(fn) {
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
+ 
 
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+	.option('-u, --url <html_url>', 'Url of index.html')
+	.parse(process.argv);
+    var symbols = process.argv;
+    console.log(symbols[4]);
+    if(symbols[4]=='--url'){
+	console.log('url...');
+	var checkJson = checkURL(program.url,program.checks);
+    }else{
+	console.log('html...');
+	var checkJson = checkHtmlFile(program.file, program.checks);}
+
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
